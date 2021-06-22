f6a574264382d6c7c4f202602a0f31fcf6bfed97
elnurruby <elnurruby@gmail.com>
2021-06-22 14:07:20 +0400

    method for increment is created

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    }
+    public static int  increase(int a){
+        a++;
+        return a;
     }
 }
